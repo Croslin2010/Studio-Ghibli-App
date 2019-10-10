a9118e51720bde224f3acdcd3811ffd7ddc64535
Croslin D. Johnson <croslin.johnson@centre.edu>
2019-10-10 15:41:32 -0400

    start project

--- code.js
+++ code.js
@@ -1,1 +1,21 @@
+var infoPromise=d3.json("https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe")
 
+infoPromise.then(
+        function(info)
+        {
+            console.log("info", info)
+            d3.select("#header").text ("Avaliable Movies:");
+        },
+        function(error)
+        {
+            console.log("none", error)
+            d3.select("#header").text("No Current Movies");
+        })
+
+var displayStuff=function(movie)
+    {
+        var box=d3.select('#clickerBox')
+            box.append("div")
+            .text("Title: " + movie.title);
+    }
+displayStuff(movie);
